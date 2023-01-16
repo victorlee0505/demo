fea8181a6af35ab8710f71c6f650e2267e98c56a
Lee <victor.k.lee@ontario.ca>
2023-01-16 09:22:31 -0500

    ApiResponse.java

---
+++ src/main/java/com/example/demo/model/ApiResponse.java
@@ -0,0 +1,8 @@
+package com.example.demo.model;
+
+public class ApiResponse {
+    private String service;
+    private String status;
+    private String statusCode;
+    private String message;
+}
