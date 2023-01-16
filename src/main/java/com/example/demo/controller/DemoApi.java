f531a0a15f282085218c82a0b1a8cc67aedee9dc
Lee <victor.k.lee@ontario.ca>
2023-01-16 09:26:59 -0500

    DemoApi: third api Post with input and use ApiResponse

--- src/main/java/com/example/demo/controller/DemoApi.java
+++ src/main/java/com/example/demo/controller/DemoApi.java
@@ -7,8 +7,12 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.ApiResponse;
 
 @RestController
 @RequestMapping("/api")
@@ -51,5 +55,26 @@ public class DemoApi {
 
         System.out.println("/api/greeting/get Finished");
         return resEnt;
+    }
+
+    /**
+     * Default greeting as JSON
+     * http://localhost:8080/api/greeting/post
+     * @return
+     */
+    @PostMapping(value = { "/greeting/post" }, produces = "application/json")
+    public ResponseEntity<ApiResponse> postGreeting(@RequestBody String name) {
+        System.out.println("/api/greeting/post Started");
+
+        String service = "/api/greeting/post";
+        String status = "Sucess";
+        String statusCode = "200";
+        System.out.println("name : " + name);
+        String message = "Hello World from " + name;
+        
+        ApiResponse response = ApiResponse.builder().service(service).status(status).statusCode(statusCode).message(message).build();
+
+        System.out.println("/api/greeting/get Finished");
+        return ResponseEntity.ok(response);
     }
 }
