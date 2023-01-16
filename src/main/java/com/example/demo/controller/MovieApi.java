7d558c964541a262f7de6328d553113fdba935f0
Lee <victor.k.lee@ontario.ca>
2023-01-16 11:16:51 -0500

    MovieApi: findById & Add

--- src/main/java/com/example/demo/controller/MovieApi.java
+++ src/main/java/com/example/demo/controller/MovieApi.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +31,44 @@ public class MovieApi {
         List<Movie> movies = movieRepository.findAll();
 
         return movies;
+
+    }
+    
+    /**
+     * Default greeting as String
+     * http://localhost:8080/api/movie/find/1
+     */
+    @GetMapping(value = { "/find/{id}" }, produces = "application/json")
+    public Movie getById(@PathVariable("id") String id) {
+
+        Optional<Movie> movie = movieRepository.findById(Long.valueOf(id));
+
+        if(movie.isPresent()){
+            return movie.get();
+        }
+
+        return null;
+
+    }
+
+
+    /**
+     * Default greeting as String
+     * http://localhost:8080/api/movie/find/1
+     */
+    @PostMapping(value = { "/add" }, produces = "application/json")
+    public Movie getById(final @RequestBody(required = true) Movie newMovie) {
+
+        Movie saved = movieRepository.saveAndFlush(newMovie);
+        Long newId = saved.getId();
+        System.out.println("New ID: " + newId);
+        Optional<Movie> movie = movieRepository.findById(newId);
+
+        if(movie.isPresent()){
+            return movie.get();
+        }
+
+        return null;
 
     }
 }
