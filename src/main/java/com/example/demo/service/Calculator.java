4f0c25505b760a62e64bafe121ffb739fe318de8
Lee <victor.k.lee@ontario.ca>
2023-01-16 10:06:52 -0500

    Calculator: simple java class

---
+++ src/main/java/com/example/demo/service/Calculator.java
@@ -0,0 +1,66 @@
+package com.example.demo.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Calculator {
+    private double leftOperand;
+    private double rightOperand;
+    private String operator;
+
+    public double calculateResult() {
+
+        double result = 0;
+        switch(this.operator) {
+            case "+":
+                result = this.leftOperand + this.rightOperand;
+                break;
+            case "-":
+                result = this.leftOperand - this.rightOperand;
+                break;
+            case "*":
+                result = this.leftOperand * this.rightOperand;
+                break;
+            case "/":
+                result = this.leftOperand / this.rightOperand;
+                break;
+            case "^":
+                result = Math.pow(this.leftOperand,this.rightOperand);
+                break;
+            default:
+                result = 0;
+        }
+        return result;
+    }
+
+
+    public double parsetring2Double4LeftNumber(String leftOperand) {
+		double leftNumber;
+		try {
+			leftNumber = Double.parseDouble(leftOperand);
+			System.out.println("Left Operand : " + leftNumber);
+		} catch (NumberFormatException ex) {
+			leftNumber = 0.0;
+			System.out.println("Left Operand Exception");
+		}
+		return leftNumber;
+	}
+
+	public double parsetring2Double4rightNumber(String rightOperand) {
+		double rightNumber;
+		try {
+			rightNumber = Double.parseDouble(rightOperand);
+			System.out.println("Right Operand : " + rightOperand);
+		} catch (NumberFormatException ex) {
+			rightNumber = 0.0;
+			System.out.println("Right Operand Exception");
+		}
+		return rightNumber;
+	}
+}
