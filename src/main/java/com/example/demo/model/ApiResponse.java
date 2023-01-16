f9b051d021961140c9da7151f6456d8922a02710
Lee <victor.k.lee@ontario.ca>
2023-01-16 09:24:26 -0500

    ApiResponse: boilerplate cont getter setter

--- src/main/java/com/example/demo/model/ApiResponse.java
+++ src/main/java/com/example/demo/model/ApiResponse.java
@@ -5,4 +5,48 @@ public class ApiResponse {
     private String status;
     private String statusCode;
     private String message;
+    
+    public ApiResponse() {
+    }
+
+    public ApiResponse(String service, String status, String statusCode, String message) {
+        this.service = service;
+        this.status = status;
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    
 }
