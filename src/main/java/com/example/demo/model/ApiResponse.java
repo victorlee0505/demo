7b8b468b5d050dee812557049a3a7db1ebba82f7
Lee <victor.k.lee@ontario.ca>
2023-01-16 09:25:41 -0500

    ApiResponse: lombok

--- src/main/java/com/example/demo/model/ApiResponse.java
+++ src/main/java/com/example/demo/model/ApiResponse.java
@@ -1,52 +1,20 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class ApiResponse {
     private String service;
     private String status;
     private String statusCode;
     private String message;
-    
-    public ApiResponse() {
-    }
 
-    public ApiResponse(String service, String status, String statusCode, String message) {
-        this.service = service;
-        this.status = status;
-        this.statusCode = statusCode;
-        this.message = message;
-    }
-
-    public String getService() {
-        return service;
-    }
-
-    public void setService(String service) {
-        this.service = service;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(String statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    
 }
